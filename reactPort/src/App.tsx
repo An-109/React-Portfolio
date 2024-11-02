53d01097d5504764a1e0b57150b29cca3ac742dd
An-109 <doggod232471@gmail.com>
2024-11-02 00:44:17 -0600

    commit

---
+++ reactPort/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1></h1>
+      <div className="">
+       
+      </div>
+      <p className="">
+        
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ reactPort/src/components/form.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export default function formFactor(){
+    return(
+       <>
+            <form>
+                
+            </form>
+       </>
+    )
+}
