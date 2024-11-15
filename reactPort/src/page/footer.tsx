2638a0b200ae8772b3622780b3cd481f8da3f232
An-109 <doggod232471@gmail.com>
2024-11-14 20:43:17 -0700

    commit

--- reactPort/src/page/footer.tsx
+++ reactPort/src/page/footer.tsx
@@ -7,10 +7,10 @@ export default function Fatter (){
         <img src="./src/assets/github-mark.png" alt="git hub"className="footerImage"/>
     </a>
     <a href="https://linkedin.com/in/an-le-198b06333 " target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/link.png" alt="linkedIn" className="footerImage"/>
+        <img src="./src/assets/link.png" alt="linkedIn" className="footerImage2"/>
     </a>
     <a href="https://instagram.com/doing.thingys/ " target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/insta.png" alt="Instagram"className="footerImage"/>
+        <img src="./src/assets/insta.png" alt="Instagram"className="footerImage2"/>
     </a>
 </footer>
     );
